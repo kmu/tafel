2e8ce3c775652b789376eb863f0921eea74dd755
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2024-11-29 10:38:59 +0900

    feat: Refactor core reader methods
    - Add new methods and imports to `reader.py` in the `core` module
    - The new methods include calculations for potential shift, logarithm of current density, current density, Tafel plot, and IR corrected potential.
    - These additions improve the functionality and capabilities of the `reader.py` module.

--- src/tafel/core/reader.py
+++ src/tafel/core/reader.py
@@ -1,10 +1,21 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 
 class Reader:
-    def read_mpt(self: "Reader", path: str) -> None:
+    def __init__(
+        self,
+        ph: float = 13,
+        ag_agcl_3m_nacl_v_vs_nhe: float = 0.222,
+        electrolyte_resistance: float = 0.05,
+    ) -> None:
+        self.ph = ph
+        self.ag_agcl_3m_nacl_v_vs_nhe = ag_agcl_3m_nacl_v_vs_nhe
+        self.electrolyte_resistance = electrolyte_resistance
+
+    def read_mpt(self, path: str) -> None:
         with Path(path).open() as f:
             contents = f.read()
 
@@ -15,3 +26,33 @@ class Reader:
 
         contents = contents.split("Electrode surface area : ")[1].split(" cm2")[0]
         self.electrode_surface_area = float(contents)
+
+    def get_potential_shift(self) -> float:
+        return self.ph * 0.0591 + self.ag_agcl_3m_nacl_v_vs_nhe
+
+    def get_log_j(self) -> pd.Series:
+        j = self.get_j()
+        return np.log10(j / 1000)
+
+    def get_j(self, cycle_number: int = -1) -> pd.Series:
+        sdf = (
+            self.df[self.df["cycle number"] == cycle_number]
+            if cycle_number >= 0
+            else self.df
+        )
+        return sdf["<I>/mA"] / self.electrode_surface_area  # mA/cm2
+
+    def get_tafel_plot(self) -> tuple:
+        logj = self.get_log_j()
+        ircp = self.get_ir_corrected_potential()
+
+        return logj, ircp
+
+    def get_ir_corrected_potential(self) -> pd.Series:
+        potential_shift = self.get_potential_shift()
+        self.E_vs_RHE_V = self.df["Ewe/V"] + potential_shift
+
+        ia = self.df["<I>/mA"] / 1000
+        self.iR = ia * self.electrolyte_resistance
+
+        return self.E_vs_RHE_V - self.iR
