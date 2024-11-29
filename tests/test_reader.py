ba2214a7441887422ef3ba782bf192313f74207d
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2024-11-29 10:40:56 +0900

    test: Update `test_reader.py` for additional tests and assertions
    - Add additional arguments when initializing `Reader`
    - Assert `reader.electrode_surface_area` is `0.046`
    - Assert potential shift within tolerance limit

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -3,5 +3,10 @@ from tafel.core.reader import Reader
 
 class TestReader:
     def test_read_mpt(self):
-        reader = Reader()
+        reader = Reader(
+            ph=13.5, ag_agcl_3m_nacl_v_vs_nhe=0.4, electrolyte_resistance=0.1
+        )
         reader.read_mpt("tests/data/example.mpt")
+
+        assert reader.electrode_surface_area == 0.046
+        assert abs(reader.get_potential_shift() - 1.19785) < 1e-5
