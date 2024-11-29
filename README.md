81df7333d49a6cde72e3f76ad4a8159cbff2bd07
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2024-11-29 16:36:30 +0900

    chore: Update test files, README, and pytest command
    - Update argument name `ag_agcl_3m_nacl_v_vs_nhe` to `reference_potential` in `Reader` initialization
    - Update test assertion for `get_potential_shift()` to use a numeric tolerance of `1e-5`
    - Added a section for getting started in the README.md file
    - Added code for installing dependencies in the getting started section
    - Updated the pytest command to specify a coverage target of "tafel.core" instead of just "tafel"

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ A command line tool to extract Tafel slope from given mpt file.
 
 ## For developers
 
+### Getting started
+
+```
+pdm install
+```
+
 ### Check code quality
 
 ```
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -3,9 +3,7 @@ from tafel.core.reader import Reader
 
 class TestReader:
     def test_read_mpt(self):
-        reader = Reader(
-            ph=13.5, ag_agcl_3m_nacl_v_vs_nhe=0.4, electrolyte_resistance=0.1
-        )
+        reader = Reader(ph=13.5, reference_potential=0.4, electrolyte_resistance=0.1)
         reader.read_mpt("tests/data/example.mpt")
 
         assert reader.electrode_surface_area == 0.046
