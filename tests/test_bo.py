e3b2a05799f555b311266d04c6eb1782468e1240
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2025-02-12 12:26:33 +0900

    Refactor code and update dependencies.
    - Import `shutil` and `Path` modules in `test_bo.py`.
    - Change the import statement for `BayesianOptimizer` in `test_bo.py`.
    - Replace the call to `perform_bayesian_optimization` with the creation of a `BayesianOptimizer` instance and invoking the `fit` method in `test_bo.py`.
    - Add a tear down method to remove the "test_output" directory using `shutil.rmtree` in `test_bo.py`.
    - Remove "ruff" from the groups list in `pdm.lock`.
    - Update the content hash for "pdm.lock".
    - Add "kaleido" package with version "0.2.1" and its dependencies in `pdm.lock`.
    - Add files and hashes for the "kaleido" package in `pdm.lock`.
    - Remove the import statement for `plotly.io` package as it is not used in the code in `bo.py`.
    - Update the method `pio.write_image` to `tafel_fig.write_image` for saving images in `bo.py`.
    - Update the `overlay_path` variable to `study_path` for saving the study data in `bo.py`.
    - Update `study_r2_` and `study_num_` to `contour_` for saving study figures in `bo.py`.
    - Remove the try-except block for writing contour figures as it is no longer necessary in `bo.py`.
    - Add the dependency "kaleido==0.2.1" to the `dependencies` list in `pyproject.toml`.

--- tests/test_bo.py
+++ tests/test_bo.py
@@ -1,6 +1,8 @@
+import shutil
+from pathlib import Path
 from unittest import TestCase
 
-from tafel.core.bo import perform_bayesian_optimization
+from tafel.core.bo import BayesianOptimizer
 from tafel.core.reader import Reader
 
 
@@ -10,4 +12,8 @@ class TestBO(TestCase):
         reader.read_mpt("tests/data/example.mpt")
         x = reader.get_log_j()
         y = reader.get_ir_corrected_potential()
-        perform_bayesian_optimization(x, y, 10, 0.5, 2, 1, [])
+        bo = BayesianOptimizer(10, 0.5, 2, 1, [], Path("test_output"))
+        bo.fit(x, y)
+
+    def tearDown(self):
+        shutil.rmtree("test_output")
